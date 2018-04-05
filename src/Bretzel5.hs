f193915dd7aec421d316925c7b054f0b463f7bfa
Stéphane Laurent <stephane.x.laurent@gsk.com>
2018-04-05 19:43:56 +0200

    bretzel5

---
+++ src/Bretzel5.hs
@@ -0,0 +1,109 @@
+module Bretzel5
+  ( main )
+  where
+import           Data.IORef
+import           Graphics.Rendering.OpenGL.GL
+import           Graphics.UI.GLUT
+import           MarchingCubes
+import           Utils.OpenGL
+
+red :: Color4 GLfloat
+red = Color4 1 0 0 1
+
+fBretzel5 :: XYZ -> Double
+fBretzel5 (x,y,z) = ((x*x+y*y/4-1)*(x*x/4+y*y-1))**2 + z*z
+
+trianglesBretzel5 :: [NTriangle]
+trianglesBretzel5 =
+  let triangles = marchingCubes fBretzel5 0.1 ((-3,3),(-3,3),(-1,1)) 200
+  in map fromTriangle triangles
+
+display :: IORef GLfloat -> IORef GLfloat -> IORef GLfloat -- rotations
+        -> IORef Double  -- zoom
+        -> DisplayCallback
+display rot1 rot2 rot3 zoom = do
+  clear [ColorBuffer, DepthBuffer]
+  r1 <- get rot1
+  r2 <- get rot2
+  r3 <- get rot3
+  z <- get zoom
+  (_, size) <- get viewport
+  let triangles = trianglesBretzel5
+  loadIdentity
+  resize z size
+  rotate r1 $ Vector3 1 0 0
+  rotate r2 $ Vector3 0 1 0
+  rotate r3 $ Vector3 0 0 1
+  renderPrimitive Triangles $ do
+    materialDiffuse Front $= red
+    mapM_ drawTriangle triangles
+  swapBuffers
+  where
+    drawTriangle ((v1,v2,v3), norm) = do
+      normal norm
+      vertex v1
+      vertex v2
+      vertex v3
+
+resize :: Double -> Size -> IO ()
+resize zoom s@(Size w h) = do
+  viewport $= (Position 0 0, s)
+  matrixMode $= Projection
+  loadIdentity
+  perspective 45.0 (w'/h') 1.0 100.0
+  lookAt (Vertex3 0 0 (-6+zoom)) (Vertex3 0 0 0) (Vector3 0 1 0)
+  matrixMode $= Modelview 0
+  where
+    w' = realToFrac w
+    h' = realToFrac h
+
+keyboard :: IORef GLfloat -> IORef GLfloat -> IORef GLfloat -- rotations
+         -> IORef Double -- zoom
+         -> KeyboardCallback
+keyboard rot1 rot2 rot3 zoom c _ = do
+  case c of
+    'e' -> rot1 $~! subtract 2
+    'r' -> rot1 $~! (+ 2)
+    't' -> rot2 $~! subtract 2
+    'y' -> rot2 $~! (+ 2)
+    'u' -> rot3 $~! subtract 2
+    'i' -> rot3 $~! (+ 2)
+    'm' -> zoom $~! (+ 1)
+    'l' -> zoom $~! subtract 1
+    'q' -> leaveMainLoop
+    _   -> return ()
+  postRedisplay Nothing
+
+main :: IO ()
+main = do
+  _ <- getArgsAndInitialize
+  _ <- createWindow "Bretzel 5"
+  windowSize $= Size 500 500
+  initialDisplayMode $= [RGBAMode, DoubleBuffered, WithDepthBuffer]
+  cullFace $= Just Back
+  clearColor $= white
+  materialAmbient Front $= white
+  lighting $= Enabled
+  lightModelTwoSide $= Disabled
+  light (Light 0) $= Enabled
+  position (Light 0) $= Vertex4 0 0 (-100) 1
+  ambient (Light 0) $= black
+  diffuse (Light 0) $= white
+  specular (Light 0) $= white
+  depthFunc $= Just Less
+  shadeModel $= Smooth
+  rot1 <- newIORef 0.0
+  rot2 <- newIORef 0.0
+  rot3 <- newIORef 0.0
+  zoom <- newIORef 0.0
+  displayCallback $= display rot1 rot2 rot3 zoom
+  reshapeCallback $= Just (resize 0)
+  keyboardCallback $= Just (keyboard rot1 rot2 rot3 zoom)
+  idleCallback $= Nothing
+  putStrLn "*** Heart ***\n\
+        \    To quit, press q.\n\
+        \    Scene rotation:\n\
+        \        e, r, t, y, u, i\n\
+        \    Zoom: l, m\n\
+        \"
+  mainLoop
